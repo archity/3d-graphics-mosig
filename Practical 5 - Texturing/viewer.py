78a2e730f5eae614951d305652f47bec526e0e9f
Archit Yadav <archityadav97@gmail.com>
2021-03-08 15:13:25 +0100

    Add files & Exercise 1

---
+++ Practical 5 - Texturing/viewer.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+"""
+Python OpenGL practical application.
+"""
+# Python built-in modules
+import os                           # os function, i.e. checking file status
+from itertools import cycle
+import sys
+
+# External, non built-in modules
+import OpenGL.GL as GL              # standard Python OpenGL wrapper
+import glfw                         # lean window system wrapper for OpenGL
+import numpy as np                  # all matrix manipulations & OpenGL args
+import assimpcy                     # 3D resource loader
+
+from core import Shader, Mesh, Node, Viewer
+from transform import rotate
+
+from PIL import Image               # load images for textures
+from itertools import cycle
+
+
+# -------------- OpenGL Texture Wrapper ---------------------------------------
+class Texture:
+    """ Helper class to create and automatically destroy textures """
+    def __init__(self, tex_file, wrap_mode=GL.GL_REPEAT, min_filter=GL.GL_LINEAR,
+                 mag_filter=GL.GL_LINEAR_MIPMAP_LINEAR):
+        self.glid = GL.glGenTextures(1)
+        try:
+            # imports image as a numpy array in exactly right format
+            tex = np.asarray(Image.open(tex_file).convert('RGBA'))
+            GL.glBindTexture(GL.GL_TEXTURE_2D, self.glid)
+            GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, tex.shape[1],
+                            tex.shape[0], 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, tex)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, wrap_mode)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, wrap_mode)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, min_filter)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, mag_filter)
+            GL.glGenerateMipmap(GL.GL_TEXTURE_2D)
+            message = 'Loaded texture %s\t(%s, %s, %s, %s)'
+            print(message % (tex_file, tex.shape, wrap_mode, min_filter, mag_filter))
+        except FileNotFoundError:
+            print("ERROR: unable to load texture file %s" % tex_file)
+
+    def __del__(self):  # delete GL texture from GPU when object dies
+        GL.glDeleteTextures(self.glid)
+
+
+# -------------- Example texture plane class ----------------------------------
+class TexturedPlane(Mesh):
+    """ Simple first textured object """
+
+    def __init__(self, tex_file, shader):
+
+        vertices = 100 * np.array(
+            ((-1, -1, 0), (1, -1, 0), (1, 1, 0), (-1, 1, 0)), np.float32)
+        faces = np.array(((0, 1, 2), (0, 2, 3)), np.uint32)
+        super().__init__(shader, [vertices], faces)
+
+        loc = GL.glGetUniformLocation(shader.glid, 'diffuse_map')
+        self.loc['diffuse_map'] = loc
+
+        # interactive toggles
+        self.wrap = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
+                           GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
+        self.filter = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
+                             (GL.GL_LINEAR, GL.GL_LINEAR),
+                             (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
+        self.wrap_mode, self.filter_mode = next(self.wrap), next(self.filter)
+        self.tex_file = tex_file
+
+        # setup texture and upload it to GPU
+        self.texture = Texture(tex_file, self.wrap_mode, *self.filter_mode)
+
+    def key_handler(self, key):
+        # some interactive elements
+        if key == glfw.KEY_F6:
+            self.wrap_mode = next(self.wrap)
+            self.texture = Texture(self.tex_file, self.wrap_mode, *self.filter_mode)
+        if key == glfw.KEY_F7:
+            self.filter_mode = next(self.filter)
+            self.texture = Texture(self.tex_file, self.wrap_mode, *self.filter_mode)
+
+    def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
+        GL.glUseProgram(self.shader.glid)
+
+        # texture access setups
+        GL.glActiveTexture(GL.GL_TEXTURE0)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.texture.glid)
+        GL.glUniform1i(self.loc['diffuse_map'], 0)
+        super().draw(projection, view, model, primitives)
+
+
+
+
+
+
+# -------------- Phong rendered Mesh class -----------------------------------
+class PhongMesh(Mesh):
+    """ Mesh with Phong illumination """
+
+    def __init__(self, shader, attributes, index=None,
+                 light_dir=(1, -1, 1),   # directional light (in world coords)
+                 k_a=(0, 0, 0), k_d=(1, 1, 0), k_s=(1, 1, 1), s=16.):
+        super().__init__(shader, attributes, index)
+        
+        print(light_dir)
+        self.light_dir = light_dir
+        self.k_a, self.k_d, self.k_s, self.s = k_a, k_d, k_s, s
+
+        # retrieve OpenGL locations of shader variables at initialization
+        names = ['light_dir', 'k_a', 's', 'k_s', 'k_d', 'w_camera_position']
+
+        loc = {n: GL.glGetUniformLocation(shader.glid, n) for n in names}
+        self.loc.update(loc)
+        
+        self.i = 0
+        self.x_vals = np.linspace(-10, 10, num=500)
+
+    def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
+        GL.glUseProgram(self.shader.glid)
+
+        # self.light_dir = [(item * glfw.get_time()) % 100 for item in self.light_dir]
+        # self.light_dir[0] = self.x_vals[self.i]
+        self.light_dir = (self.x_vals[self.i], 0, -1)
+        self.i = (self.i + 1) % 500
+        # setup light parameters
+        GL.glUniform3fv(self.loc['light_dir'], 1, self.light_dir)
+
+        # setup material parameters
+        GL.glUniform3fv(self.loc['k_a'], 1, self.k_a)
+        GL.glUniform3fv(self.loc['k_d'], 1, self.k_d)
+        GL.glUniform3fv(self.loc['k_s'], 1, self.k_s)
+        GL.glUniform1f(self.loc['s'], max(self.s, 0.001))
+
+        # world camera position for Phong illumination specular component
+        w_camera_position = np.linalg.inv(view)[:,3]
+        GL.glUniform3fv(self.loc['w_camera_position'], 1, w_camera_position)
+
+        super().draw(projection, view, model, primitives)
+
+
+# -------------- 3D resource loader -----------------------------------------
+def load_phong_mesh(file, shader, light_dir):
+    """ load resources from file using assimp, return list of ColorMesh """
+    try:
+        pp = assimpcy.aiPostProcessSteps
+        flags = pp.aiProcess_Triangulate | pp.aiProcess_GenSmoothNormals
+        scene = assimpcy.aiImportFile(file, flags)
+    except assimpcy.all.AssimpError as exception:
+        print('ERROR loading', file + ': ', exception.args[0].decode())
+        return []
+
+    # prepare mesh nodes
+    meshes = []
+    for mesh in scene.mMeshes:
+        mat = scene.mMaterials[mesh.mMaterialIndex].properties
+        mesh = PhongMesh(shader, [mesh.mVertices, mesh.mNormals], mesh.mFaces,
+                         k_d=mat.get('COLOR_DIFFUSE', (1, 1, 1)),
+                         k_s=mat.get('COLOR_SPECULAR', (1, 1, 1)),
+                         k_a=mat.get('COLOR_AMBIENT', (0, 0, 0)),
+                         s=mat.get('SHININESS', 16.),
+                         light_dir=light_dir)
+        meshes.append(mesh)
+
+    size = sum((mesh.mNumFaces for mesh in scene.mMeshes))
+    print('Loaded %s\t(%d meshes, %d faces)' % (file, len(meshes), size))
+    return meshes
+
+
+# -------------- main program and scene setup --------------------------------
+def main():
+    """ create a window, add scene objects, then run rendering loop """
+    viewer = Viewer()
+    shader = Shader("texture.vert", "texture.frag")
+
+    node = Node(transform=rotate((0, 0, 1), 45))
+    viewer.add(node)
+
+    # light_dir = (0, 0, -10)
+    # node.add(*[mesh for file in sys.argv[1:]
+    #            for mesh in load_phong_mesh(file, shader, light_dir)])
+
+
+
+    plane = TexturedPlane("./grass.png", shader)
+    viewer.add(plane)
+
+
+
+    if len(sys.argv) != 2:
+        print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
+              ' format supported by assimp.' % (sys.argv[0],))
+
+    # start rendering loop
+    viewer.run()
+
+
+if __name__ == '__main__':
+    glfw.init()                # initialize window system glfw
+    main()                     # main function keeps variables locally scoped
+    glfw.terminate()           # destroy all glfw windows and GL contexts
