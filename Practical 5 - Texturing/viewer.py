a903ae089aeaf7da7f896b1b0474d371c777d486
Archit Yadav <archityadav97@gmail.com>
2021-03-12 01:35:04 +0100

    Exercise 2

--- Practical 5 - Texturing/viewer.py
+++ Practical 5 - Texturing/viewer.py
@@ -92,75 +92,76 @@ class TexturedPlane(Mesh):
 
 
 
+class TexturedMesh(Mesh):
 
+    def __init__(self, shader, tex, attributes, faces):
+        super().__init__(shader, attributes, faces)
 
+        loc = GL.glGetUniformLocation(shader.glid, 'diffuse_map')
+        self.loc['diffuse_map'] = loc
 
-# -------------- Phong rendered Mesh class -----------------------------------
-class PhongMesh(Mesh):
-    """ Mesh with Phong illumination """
+        # interactive toggles
+        self.wrap = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
+                           GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
+        self.filter = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
+                             (GL.GL_LINEAR, GL.GL_LINEAR),
+                             (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
+        self.wrap_mode, self.filter_mode = next(self.wrap), next(self.filter)
 
-    def __init__(self, shader, attributes, index=None,
-                 light_dir=(1, -1, 1),   # directional light (in world coords)
-                 k_a=(0, 0, 0), k_d=(1, 1, 0), k_s=(1, 1, 1), s=16.):
-        super().__init__(shader, attributes, index)
-        
-        print(light_dir)
-        self.light_dir = light_dir
-        self.k_a, self.k_d, self.k_s, self.s = k_a, k_d, k_s, s
+        # setup texture and upload it to GPU
+        self.texture = tex
 
-        # retrieve OpenGL locations of shader variables at initialization
-        names = ['light_dir', 'k_a', 's', 'k_s', 'k_d', 'w_camera_position']
-
-        loc = {n: GL.glGetUniformLocation(shader.glid, n) for n in names}
-        self.loc.update(loc)
-        
-        self.i = 0
-        self.x_vals = np.linspace(-10, 10, num=500)
+    def key_handler(self, key):
+        # some interactive elements
+        if key == glfw.KEY_F6:
+            self.wrap_mode = next(self.wrap)
+            self.texture = Texture(self.tex_file, self.wrap_mode, *self.filter_mode)
+        if key == glfw.KEY_F7:
+            self.filter_mode = next(self.filter)
+            self.texture = Texture(self.tex_file, self.wrap_mode, *self.filter_mode)
 
     def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
         GL.glUseProgram(self.shader.glid)
 
-        # self.light_dir = [(item * glfw.get_time()) % 100 for item in self.light_dir]
-        # self.light_dir[0] = self.x_vals[self.i]
-        self.light_dir = (self.x_vals[self.i], 0, -1)
-        self.i = (self.i + 1) % 500
-        # setup light parameters
-        GL.glUniform3fv(self.loc['light_dir'], 1, self.light_dir)
-
-        # setup material parameters
-        GL.glUniform3fv(self.loc['k_a'], 1, self.k_a)
-        GL.glUniform3fv(self.loc['k_d'], 1, self.k_d)
-        GL.glUniform3fv(self.loc['k_s'], 1, self.k_s)
-        GL.glUniform1f(self.loc['s'], max(self.s, 0.001))
-
-        # world camera position for Phong illumination specular component
-        w_camera_position = np.linalg.inv(view)[:,3]
-        GL.glUniform3fv(self.loc['w_camera_position'], 1, w_camera_position)
-
+        # texture access setups
+        GL.glActiveTexture(GL.GL_TEXTURE0)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.texture.glid)
+        GL.glUniform1i(self.loc['diffuse_map'], 0)
         super().draw(projection, view, model, primitives)
 
 
-# -------------- 3D resource loader -----------------------------------------
-def load_phong_mesh(file, shader, light_dir):
-    """ load resources from file using assimp, return list of ColorMesh """
+
+def load_textured(file, shader, tex_file=None):
+    """ load resources from file using assimp, return list of TexturedMesh """
     try:
         pp = assimpcy.aiPostProcessSteps
-        flags = pp.aiProcess_Triangulate | pp.aiProcess_GenSmoothNormals
+        flags = pp.aiProcess_Triangulate | pp.aiProcess_FlipUVs
         scene = assimpcy.aiImportFile(file, flags)
     except assimpcy.all.AssimpError as exception:
         print('ERROR loading', file + ': ', exception.args[0].decode())
         return []
 
-    # prepare mesh nodes
+    # Note: embedded textures not supported at the moment
+    path = os.path.dirname(file) if os.path.dirname(file) != '' else './'
+    for mat in scene.mMaterials:
+        if not tex_file and 'TEXTURE_BASE' in mat.properties:  # texture token
+            name = os.path.basename(mat.properties['TEXTURE_BASE'])
+            # search texture in file's whole subdir since path often screwed up
+            paths = os.walk(path, followlinks=True)
+            found = [os.path.join(d, f) for d, _, n in paths for f in n
+                     if name.startswith(f) or f.startswith(name)]
+            assert found, 'Cannot find texture %s in %s subtree' % (name, path)
+            tex_file = found[0]
+        if tex_file:
+            mat.properties['diffuse_map'] = Texture(tex_file=tex_file)
+
+    # prepare textured mesh
     meshes = []
     for mesh in scene.mMeshes:
         mat = scene.mMaterials[mesh.mMaterialIndex].properties
-        mesh = PhongMesh(shader, [mesh.mVertices, mesh.mNormals], mesh.mFaces,
-                         k_d=mat.get('COLOR_DIFFUSE', (1, 1, 1)),
-                         k_s=mat.get('COLOR_SPECULAR', (1, 1, 1)),
-                         k_a=mat.get('COLOR_AMBIENT', (0, 0, 0)),
-                         s=mat.get('SHININESS', 16.),
-                         light_dir=light_dir)
+        assert mat['diffuse_map'], "Trying to map using a textureless material"
+        attributes = [mesh.mVertices, mesh.mTextureCoords[0]]
+        mesh = TexturedMesh(shader, mat['diffuse_map'], attributes, mesh.mFaces)
         meshes.append(mesh)
 
     size = sum((mesh.mNumFaces for mesh in scene.mMeshes))
@@ -177,20 +178,23 @@ def main():
     node = Node(transform=rotate((0, 0, 1), 45))
     viewer.add(node)
 
-    # light_dir = (0, 0, -10)
-    # node.add(*[mesh for file in sys.argv[1:]
-    #            for mesh in load_phong_mesh(file, shader, light_dir)])
+    # Grass
+    # plane = TexturedPlane(".resources/grass.png", shader)
+    # viewer.add(plane)
 
+    
+    # if len(sys.argv) != 2:
+    #     print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
+    #           ' format supported by assimp.' % (sys.argv[0],))
 
+    # Get the list of meshes from the object
+    mesh_list = load_textured("./resources/cube/cube.obj", shader=shader)
 
-    plane = TexturedPlane("./grass.png", shader)
-    viewer.add(plane)
+    for mesh in mesh_list:
+        viewer.add(mesh)
 
-
-
-    if len(sys.argv) != 2:
-        print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
-              ' format supported by assimp.' % (sys.argv[0],))
+    # Alternate one-line command
+    # viewer.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file, shader=shader)])
 
     # start rendering loop
     viewer.run()
