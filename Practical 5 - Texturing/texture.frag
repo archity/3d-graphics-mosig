0fc9370c6297f4cd9cda1e0e809d6545e0944059
Archit Yadav <archityadav97@gmail.com>
2021-04-04 16:04:14 +0200

    Correct object texturing

--- Practical 5 - Texturing/texture.frag
+++ Practical 5 - Texturing/texture.frag
@@ -2,8 +2,9 @@
 
 uniform sampler2D diffuse_map;
 in vec2 frag_tex_coords;
+in vec2 frag_uv;
 out vec4 out_color;
 
 void main() {
-    out_color = texture(diffuse_map, frag_tex_coords);
+    out_color = texture(diffuse_map, frag_uv);
 }
--- Practical 5 - Texturing/viewer.py
+++ Practical 5 - Texturing/viewer.py
@@ -100,25 +100,9 @@ class TexturedMesh(Mesh):
         loc = GL.glGetUniformLocation(shader.glid, 'diffuse_map')
         self.loc['diffuse_map'] = loc
 
-        # interactive toggles
-        self.wrap = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
-                           GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
-        self.filter = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
-                             (GL.GL_LINEAR, GL.GL_LINEAR),
-                             (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
-        self.wrap_mode, self.filter_mode = next(self.wrap), next(self.filter)
-
         # setup texture and upload it to GPU
         self.texture = tex
 
-    def key_handler(self, key):
-        # some interactive elements
-        if key == glfw.KEY_F6:
-            self.wrap_mode = next(self.wrap)
-            self.texture = Texture(self.tex_file, self.wrap_mode, *self.filter_mode)
-        if key == glfw.KEY_F7:
-            self.filter_mode = next(self.filter)
-            self.texture = Texture(self.tex_file, self.wrap_mode, *self.filter_mode)
 
     def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
         GL.glUseProgram(self.shader.glid)
@@ -128,6 +112,10 @@ class TexturedMesh(Mesh):
         GL.glBindTexture(GL.GL_TEXTURE_2D, self.texture.glid)
         GL.glUniform1i(self.loc['diffuse_map'], 0)
         super().draw(projection, view, model, primitives)
+
+        # leave clean state for easier debugging
+        GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
+        GL.glUseProgram(0)
 
 
 
@@ -169,32 +157,75 @@ def load_textured(file, shader, tex_file=None):
     return meshes
 
 
+# -------------- Phong rendered Mesh class -----------------------------------
+class PhongMesh(Mesh):
+    """ Mesh with Phong illumination """
+
+    def __init__(self, shader, attributes, index=None,
+                 light_dir=(0, -1, 0),   # directional light (in world coords)
+                 k_a=(0, 0, 0), k_d=(1, 1, 0), k_s=(1, 1, 1), s=16.):
+        super().__init__(shader, attributes, index)
+        self.light_dir = light_dir
+        self.k_a, self.k_d, self.k_s, self.s = k_a, k_d, k_s, s
+
+        # retrieve OpenGL locations of shader variables at initialization
+        names = ['light_dir', 'k_a', 's', 'k_s', 'k_d', 'w_camera_position']
+
+        loc = {n: GL.glGetUniformLocation(shader.glid, n) for n in names}
+        self.loc.update(loc)
+
+    def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
+        GL.glUseProgram(self.shader.glid)
+
+        # setup light parameters
+        GL.glUniform3fv(self.loc['light_dir'], 1, self.light_dir)
+
+        # setup material parameters
+        GL.glUniform3fv(self.loc['k_a'], 1, self.k_a)
+        GL.glUniform3fv(self.loc['k_d'], 1, self.k_d)
+        GL.glUniform3fv(self.loc['k_s'], 1, self.k_s)
+        GL.glUniform1f(self.loc['s'], max(self.s, 0.001))
+
+        # world camera position for Phong illumination specular component
+        w_camera_position = np.linalg.inv(view)[:,3]
+        GL.glUniform3fv(self.loc['w_camera_position'], 1, w_camera_position)
+
+        super().draw(projection, view, model, primitives)
+
+
+
 # -------------- main program and scene setup --------------------------------
 def main():
     """ create a window, add scene objects, then run rendering loop """
     viewer = Viewer()
     shader = Shader("texture.vert", "texture.frag")
 
-    node = Node(transform=rotate((0, 0, 1), 45))
-    viewer.add(node)
+    # node = Node(transform=rotate((0, 0, 1), 45))
+    # viewer.add(node)
 
     # Grass
-    # plane = TexturedPlane(".resources/grass.png", shader)
+    # plane = TexturedPlane("./resources/grass.png", shader)
     # viewer.add(plane)
 
     
-    # if len(sys.argv) != 2:
-    #     print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
-    #           ' format supported by assimp.' % (sys.argv[0],))
+    if len(sys.argv) != 2:
+        print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
+              ' format supported by assimp.' % (sys.argv[0],))
 
+    # Wall entrance
     # Get the list of meshes from the object
-    mesh_list = load_textured("./resources/cube/cube.obj", shader=shader)
+    # mesh_list = load_textured("./resources/tall_wall_entrance/TallWallEntrance.obj", shader=shader, tex_file="./resources/tower/castle_wall.jpg")
+    # for mesh in mesh_list:
+    #     viewer.add(mesh)
 
-    for mesh in mesh_list:
-        viewer.add(mesh)
+    # Bridge
+    # bridge_mesh_list = load_textured("./resources/bridge/Bridge.obj", shader=shader, tex_file="./resources/bridge/wooden_texture.jpg")
+    # for mesh in bridge_mesh_list:
+    #     viewer.add(mesh)
+
 
     # Alternate one-line command
-    # viewer.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file, shader=shader)])
+    viewer.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file, shader=shader)])
 
     # start rendering loop
     viewer.run()
